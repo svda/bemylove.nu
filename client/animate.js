fdc9846fcb0b24b1a7992b124f08d9f2cb5c0b6a
Sander van den Akker <sander@myxt.nl>
2012-12-23 20:42:59 +0100

    Better flow.

--- client/animate.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$(document).on( 'click', 'a[href^="#"]', function () {
-        var target = $(this).attr("href");
-        $("html:not(:animated),body:not(:animated)").animate({
-            scrollTop: $(target).offset().top
-        }, 400 );
-        return false;
-	});
-});
